06439cdf389d79efd2e437cadaff5c7bb7033c73
Christian Giammatteo <42420087+CGiammatteo@users.noreply.github.com>
2021-08-29 13:15:45 -0400

    Add files via upload

---
+++ Port Scanner/Port Scanner/Colors.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Port_Scanner
+{
+    class Colors
+    {
+        public static void Success(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(message);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        public static void Error(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        public static void Question(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine(message);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
---
+++ Port Scanner/Port Scanner/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Port_Scanner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "TCP Port Scanner";
+
+            Console.WriteLine("Input An IP To Scan: ");
+            string ip = Console.ReadLine();
+            Console.WriteLine("Choose An Option: ");
+
+            string[] options = { "Scan Ports 1 By 1", "Scan Common TCP Ports", "Scan Every Port (0 - 65535)" };
+
+            for(int i = 0; i < options.Length; i++)
+            {
+                Console.WriteLine("[" + i + "] " + options[i]);
+            }
+
+            int answer = Convert.ToInt32(Console.ReadLine());
+
+            switch (answer)
+            {
+                case 0:
+                    while (true)
+                    {
+                        Console.WriteLine("Port: ");
+                        int port = Convert.ToInt32(Console.ReadLine());
+                        ScanPort(ip, port);
+                    }
+                case 1:
+                    int[] commonPorts = { 80, 443, 53, 25, 20, 21, 502, 102, 44818 };
+
+                    Console.WriteLine("Starting...");
+
+                    foreach (int port in commonPorts)
+                    {
+                        ScanPort(ip, port);
+                    }
+                    break;
+                case 2:
+                    Console.WriteLine("Starting...");
+
+                    for (int i = 0; i < 65535; i++)
+                    {
+                        ScanPort(ip, i);
+                    }
+                    break;
+                default:
+                    Colors.Error("Incorrect Input! Press Any Key To Close...");
+                    Console.ReadKey();
+                    Environment.Exit(0);
+                    break;
+            }
+
+            Colors.Success("Task Completed! Press Any Key To Close...");
+            Console.ReadKey();
+            Environment.Exit(0);
+        }
+
+        private static void ScanPort(string ip, int port)
+        {
+            using (TcpClient scanner = new TcpClient())
+            {
+                try
+                {
+                    scanner.Connect(IPAddress.Parse(ip), port);
+                    Colors.Success("Port " + port + " | OPEN");
+                }
+                catch (Exception ex)
+                {
+                    Colors.Error("Port " + port + " | CLOSED");
+                }
+            }
+        }
+    }
+}
